3e6d38c1106227646191225d84bf96e45b03b424
Muhan Li <github@umm.one>
2022-03-27 05:39:58 +0800

    support December of last year

--- parse/data/input/utils.go
+++ parse/data/input/utils.go
@@ -2,6 +2,7 @@ package input
 
 import (
 	"fmt"
+	"strconv"
 	"strings"
 	"time"
 )
@@ -9,10 +10,15 @@ import (
 func date(year int, date string) (result time.Time) {
 	input := fmt.Sprintf("%04d-%s", year, date)
 	result, _ = time.Parse("2006-1.2", input)
+
+	if date[0] == '0' { // => 0001-1.1
+		delta, _ := strconv.Atoi(date[2:]) // days before
+		result = result.AddDate(year-1, 0, -delta)
+	}
 	return
 }
 
-func holidays(year int, daysRaw string) (result []string) {
+func holidays(year int, daysRaw string) (result []time.Time) {
 	if daysRaw == "" {
 		return
 	}
@@ -22,10 +28,10 @@ func holidays(year int, daysRaw string) (result []string) {
 		if strings.Contains(day, "-") {
 			period := strings.Split(day, "-")
 			for d := date(year, period[0]); !d.After(date(year, period[1])); d = d.AddDate(0, 0, 1) {
-				result = append(result, d.Format("1.2"))
+				result = append(result, d)
 			}
 		} else {
-			result = append(result, day)
+			result = append(result, date(year, day))
 		}
 	}
 	return result
